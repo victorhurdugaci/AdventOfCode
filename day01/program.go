3e505f18fd56218436ab8314380f819a7de0178d
Victor Hurdugaci <contact@victorhurdugaci.com>
2015-12-20 11:07:45 -0800

    Tabs -> whitespaces

--- day01/program.go
+++ day01/program.go
@@ -1,50 +1,50 @@
 package main
 
 import (
-	"fmt"
-	"io"
-	"os"
+    "fmt"
+    "io"
+    "os"
 )
 
 func main() {
-	f, err := os.Open("input")
-	check(err) 
-	
-	buffer := make([]byte, 128)
-	
-	currentFloor := 0
-	
-	basementChar := 0
-	totalChars := 0
-	
-	for {
-		n, err := f.Read(buffer)
-		if err != nil && 
-		   err == io.EOF {
-			break
-		}
-		
-		check(err)
-		
-		for i := 0; i < n; i++ {
-			totalChars++
-			
-			if buffer[i] == '(' {
-				currentFloor++
-			} else {
-				currentFloor--
-			}
-			
-			if basementChar == 0 &&
-			   currentFloor < 0 {
-				basementChar = totalChars	   
-			}
-		}
-	}
-	f.Close()
-	
-	fmt.Printf("Basement entry char: %d\n", basementChar)
-	fmt.Printf("Final floor: %d\n", currentFloor)
+    f, err := os.Open("input")
+    check(err) 
+    
+    buffer := make([]byte, 128)
+    
+    currentFloor := 0
+    
+    basementChar := 0
+    totalChars := 0
+    
+    for {
+    n, err := f.Read(buffer)
+        if err != nil && 
+        err == io.EOF {
+            break
+        }
+        
+        check(err)
+        
+        for i := 0; i < n; i++ {
+            totalChars++
+            
+            if buffer[i] == '(' {
+                currentFloor++
+            } else {
+                currentFloor--
+            }
+            
+            if basementChar == 0 &&
+            currentFloor < 0 {
+                basementChar = totalChars	   
+            }
+        }
+    }
+    f.Close()
+    
+    fmt.Printf("Basement entry char: %d\n", basementChar)
+    fmt.Printf("Final floor: %d\n", currentFloor)
 }
 
 func check(e error) {
